9b092311e11c7096bc5929a514c5ee59232b57c6
Otavio Novais <otavio14novais@gmail.com>
2021-12-17 15:28:15 -0300

    Trocando a imagem Redes sociais por imagens separadas dos ícones

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,31 @@
-import {Top, Image1} from './styles'
+import { Botao } from '../Header/styles'
+import { Top, Image1, Icones, Bottom, Paragrafo, Span } from './styles'
 
-export default function Footer({children}) {
+export default function Footer({ children }) {
   return (
     <>
       <Top>
-         <Image1 src="/images/Logo InfoJr.svg" alt="Logo InfoJr" />
-         <Image1 src="/images/Redes sociais.svg" alt="Logo InfoJr" />
+        <Image1 src="/images/Logo InfoJr.svg" alt="Logo InfoJr" />
+      
+          <Icones src="/images/Icon_Linkedin.svg" alt="Logo InfoJr" />
+          <Icones src="/images/Icon_Behance.svg" alt="Logo InfoJr" />
+          <Icones src="/images/Icon_Dribble.svg" alt="Logo InfoJr" />
+          <Icones src="/images/Icon_Instagram.svg" alt="Logo InfoJr" />
+          <Icones src="/images/Icon_Facebook.svg" alt="Logo InfoJr" />
+          <Icones src="/images/Icon_Youtube.svg" alt="Logo InfoJr" />
+          <Icones src="/images/Icon_Spotify.svg" alt="Logo InfoJr" />
+       
       </Top>
+      <Bottom>
+        <Paragrafo>
+          <Span>Email:</Span> contato@infojr.com.br | <Span>Telefone:</Span> 71
+          3283-6268
+        </Paragrafo>
+        <Paragrafo>
+          Copyright @ 2020 INFO JR UFBA - Av. Adhemar de Barros, Ondina –
+          Instituto de computação da UFBA
+        </Paragrafo>
+      </Bottom>
     </>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -5,17 +5,50 @@ export const Top = styled.footer`
 height:240px;
 background:#252822;
 display:flex;
-justify-content: space-between;
+justify-content:space-between;
 align-items:center;
-padding:0 18%;
+padding: 0 18%;
 `
-
-
 export const Image1 = styled.img`
 width:220px;
+margin-right:28%;
+height:220px;
 `
 export const Icones = styled.img`
 height:45px;
+width:45px;
+
+
+&:hover{
+  imag-shadow: 0 0 40px #fff;
+}
 `
-  
+export const Bottom = styled.footer`
+height:124px;
+background:#191919;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+gap:10px;`
+export const Paragrafo = styled.p`
+color: #FFFFFF;
+font-family: 'Poppins', sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+text-align: center;
+
+`
+export const Span = styled.span`
+font-weight:700;
+font-size: 16px;
+line-height: 24px;
+text-align: center;
+color: #FFFFFF;
+font-family: 'Poppins', sans-serif;
+font-style: normal;
+`
+
 
